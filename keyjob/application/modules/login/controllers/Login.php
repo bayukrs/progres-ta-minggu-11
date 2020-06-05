cea0e35b3770d45460e890b1ca1f54d0cb42a371
bayukrs <bayukrismant@gmail.com>
2020-06-05 19:04:26 +0700

    progres ta minggu 11

---
+++ keyjob/application/modules/login/controllers/Login.php
@@ -0,0 +1,56 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); 
+
+class Login extends MY_Controller { 
+ 
+  	public function __construct(){     
+  		parent::__construct(); 
+ 
+    	$this->load->model('UserModel');   
+	}
+
+	public function index(){      
+		if($this->session->userdata('authenticated')) // Jika user sudah login (Session authenticated ditemukan)       
+  		redirect('home'); // Redirect ke page home 
+    	// function render_login tersebut dari file core/MY_Controller.php     
+    	$this->render_login('login'); // Load view login.php   
+    }
+
+    public function login_account(){
+    	$username = $this->input->post('username');    
+    	$password = md5($this->input->post('password')); 
+
+    	$user = $this->UserModel->get($username);
+
+    	if (empty($user)) {
+    		$this->session->set_flashdata('message', 'Username tidak ditemukan');
+
+    		redirect('login');
+    	} else{       
+    			if($password == $user->password){ // Jika password yang diinput sama dengan password yang didatabase         
+    			$session = array(           
+    				'authenticated'=>true, // Buat session authenticated dengan value true           
+    				'username'=>$user->username,  // Buat session username
+    				'level'=>$user->level, // Buat session role 
+            'active' => $user->active,        
+            'user_id' => $user->user_id
+    			); 
+ 
+        		$this->session->set_userdata($session);
+        			if ($this->session->userdata("level") == "admin") {
+                          redirect('admin');
+                       }else if ($this->session->userdata("level") == "freelancer") {
+                          redirect('dashboard/user');
+                       }else if ($this->session->userdata("level") == "member") {
+                          redirect('member');
+                       }  
+        		}else{         
+        			$this->session->set_flashdata('message', 'Password salah'); // Buat session flashdata         
+        			redirect('login'); // Redirect ke halaman login       
+        		}     
+        	}
+    }
+    public function logout(){     
+ 		$this->session->sess_destroy(); // Hapus semua session     
+ 		redirect('home'); // Redirect ke halaman login   
+ 	}
+}
